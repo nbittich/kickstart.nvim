6c29121ee758ade09936ea784fe2afcb50df7e8d
Nordine Bittich <nordine@bittich.be>
2023-09-07 14:04:51 +0200

    setup rust

--- lua/custom/plugins/rust-tools.lua
+++ lua/custom/plugins/rust-tools.lua
@@ -1,5 +1,4 @@
 return {
-
     "simrat39/rust-tools.nvim",
     dependencies = {
         "nvim-lua/plenary.nvim",
@@ -9,11 +8,13 @@ return {
         local rt = require("rust-tools")
         rt.setup({
             server = {
-                on_attach = function(_, bufnr)
-                    -- Hover actions
-                    vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-                    -- Code action groups
-                    vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+                on_attach = function(client, bufnr)
+                    if client.name == "rust_analyzer" then
+                        -- Hover actions
+                        vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+                        -- Code action groups
+                        -- vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+                    end
                 end,
             },
         })
