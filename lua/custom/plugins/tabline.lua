6f95a00776d1f748a499a51c06853f7cc4f27f90
Nordine Bittich <nordine@bittich.be>
2023-09-07 14:32:44 +0200

    ok

--- lua/custom/plugins/tabline.lua
+++ lua/custom/plugins/tabline.lua
@@ -8,12 +8,17 @@ return {
     init = function() vim.g.barbar_auto_setup = false end,
     opts = {
       -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-      animation = false,
-
-      -- insert_at_start = true,
-      -- …etc.
     },
     version = '^1.0.0', -- optional: only update when a new 1.x version is released
+
+    config = function()
+      local map = vim.api.nvim_set_keymap
+      local opts = { noremap = true, silent = true }
+      require("barbar").setup({
+        animation = false,
+        map('n', '<leader>Bc', '<Cmd>BufferClose<CR>', opts)
+      })
+    end
   },
 
 }
